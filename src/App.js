09ee64fea82f62e709ee26f2edb33a505a2c4593
dimrio323 <62644864+dimrio323@users.noreply.github.com>
2021-03-19 13:45:20 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+import './App.css'
+import {Route, Switch} from 'react-router-dom'
+import Layout from './hoc/Layout/Layout'
+import Home from './containers/Home/Home'
+
+class App extends Component {
+  render() {
+    return (
+        <Layout>
+          <Switch>
+            <Route path='/' component={Home} />
+          </Switch>
+        </Layout>
+    );
+  }
+}
+
+export default App
